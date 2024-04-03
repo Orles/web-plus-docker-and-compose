b2a43de74ad163b3144f19cddc591e81e40fac65
Иван Орлов <ivan.orel60@yandex.ru>
2024-04-03 15:26:04 +0300

    fised eerrors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
+  
+  // Устанавливаем заголовки CORS
+  app.enableCors({
+    origin: 'https://kpd.garga.nomoredomainswork.ru',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
