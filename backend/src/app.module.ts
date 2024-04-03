a66ab1ddea254915d7a2aa62cae38e307785cdea
Иван Орлов <ivan.orel60@yandex.ru>
2024-04-03 16:39:31 +0300

    fixed errors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './user/users.module';
 import { WishesModule } from './wishes/wishes.module';
@@ -10,34 +11,32 @@ import { Wishlist } from './wishlists/entities/wishlist.entity';
 import { Offer } from './offers/entities/offer.entity';
 import { HashModule } from './hash/hash.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { HashService } from './hash/hash.service';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [User, Wish, Wishlist, Offer],
-        synchronize: true,
-      }),
-      inject: [ConfigService],
-    }),
-    AuthModule,
-    HashModule,
-    OffersModule,
     UsersModule,
     WishesModule,
     WishlistsModule,
-  ],
+    OffersModule,
+    HashModule,
+    AuthModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User, Wish, Offer, Wishlist],
+      synchronize: true, 
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env']
+    }),],
+  controllers: [AppController],
+  providers: [AppService, HashService],
 })
-export class AppModule {}
+export class AppModule { }
